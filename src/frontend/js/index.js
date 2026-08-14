8b51a03d1f20da8ea2a72a3c38292e4818165d25
Lolikion228 <st117139@student.spbu.ru>
2026-08-14 16:53:57 +0300

    add world gen web interface

--- src/frontend/js/index.js
+++ src/frontend/js/index.js
@@ -3,26 +3,59 @@ const base_url = "http://localhost:8004"
 const login_button = document.getElementById('login_button');
 const register_button = document.getElementById('register_button');
 const logout_button = document.getElementById('logout_button');
-const worlds_btn = document.getElementById('worlds_btn');
 const status_text = document.getElementById('status_text');
+
 const online_info_text = document.getElementById('online_info_text');
+
+const worlds_btn = document.getElementById('worlds_btn');
+const world_seed_input = document.getElementById('world_seed_input');
+const world_gen_btn = document.getElementById('world_gen_btn');
+
+var user_id = null;
 
 async function init(){
     const result = await fetch(base_url + "/api/v1/auth/session_info");
     const data = await result.json();
-
+   
     status_text.textContent = data.msg;   
 
     if(data.is_logged){
         login_button.style.display = "none";
         register_button.style.display = "none";
         logout_button.style.display = "block";
+        world_seed_input.style.display = "block";
+        world_gen_btn.style.display = "block";
+        user_id = data.user_id;
     }
     else{
         login_button.style.display = "block";
         register_button.style.display = "block";
         logout_button.style.display = "none";
+        world_seed_input.style.display = "none";
+        world_gen_btn.style.display = "none";
     }
+
+}
+
+
+async function gen_world(){
+
+    var params = new URLSearchParams({
+            'user_id': user_id,
+    });
+
+    if(world_seed_input.value.length != 0){
+        params.append('seed', world_seed_input.value);
+    }
+    
+
+    const result = await fetch(
+        base_url + `/api/v1/admin/generate_world?${params}`,
+        {
+            method: 'POST'
+        }
+    );
+
 
 }
 
@@ -50,6 +83,11 @@ worlds_btn.addEventListener('click', async () => {
     window.location.href = base_url + "/worlds";
 });
 
-init();
-update_online_info();
+
+world_gen_btn.addEventListener('click', async () => {
+    await gen_world();
+});
+
+queueMicrotask( async() => {await init();});
+queueMicrotask( async() => {await update_online_info();});
 setInterval(update_online_info, 3000);
