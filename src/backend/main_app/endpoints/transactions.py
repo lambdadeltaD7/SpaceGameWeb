01eda876f98c68b5a39f03ef580bfab018812301
Lolikion228 <st117139@student.spbu.ru>
2026-08-15 19:42:59 +0300

    cleaning in endpoints/transactions.py

--- src/backend/main_app/endpoints/transactions.py
+++ src/backend/main_app/endpoints/transactions.py
@@ -1,5 +1,3 @@
-from fastapi import APIRouter, HTTPException, status, Depends
-
 from typing import Annotated
 
 from db_connection import sql_engine, r_sessions
@@ -7,11 +5,13 @@ from db_connection import sql_engine, r_sessions
 from sqlalchemy.orm import Session
 from sqlalchemy import select, or_, text, update, delete
 
-from endpoints.auth import get_or_create_session
 from endpoints.admin import check_admin
+from endpoints.auth import get_or_create_session
+from fastapi import APIRouter, HTTPException, status, Depends
 
-from db_models import TransactionsSchemaDB, UsersSchemaDB
 from pd_models import TransactionsSchemaPD
+from db_models import TransactionsSchemaDB, UsersSchemaDB
+
 
 router = APIRouter(prefix="/api/v1/transactions")
 
@@ -24,16 +24,16 @@ def get_transactions(
 ):
     if (user_id is None) and (not is_admin):
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="only admins can view all transactions"
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "only admins can view all transactions"
         )
     
     requester_uid = r_sessions.hget(f"ses_{session_id}", "user_id")
 
     if (user_id is not None) and (not is_admin) and (requester_uid != str(user_id)):
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="you can view only your own transactions"
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "you can view only your own transactions"
         )
 
     with Session(sql_engine) as ses:
@@ -41,12 +41,13 @@ def get_transactions(
         if user_id is not None:
             stmt = stmt.where( 
                 or_(
-                    TransactionsSchemaDB.user_from_id==user_id, 
-                    TransactionsSchemaDB.user_to_id==user_id
+                    TransactionsSchemaDB.user_from_id == user_id, 
+                    TransactionsSchemaDB.user_to_id == user_id
                 )
             )
-        result = ses.scalars(stmt).all()
-    return [t for t in result]
+        transactions = ses.scalars(stmt).all()
+
+    return [t for t in transactions]
 
 
 @router.get("/{transaction_id}")
@@ -56,21 +57,44 @@ def get_transaction(
 ):
     if not is_admin:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="only admins can view specific transactions"
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "only admins can view specific transactions"
         )
 
     with Session(sql_engine) as ses:
-        stmt = select(TransactionsSchemaDB).where(TransactionsSchemaDB.transaction_id==transaction_id)
-        result = ses.scalar(stmt)
-    if result:
-        return result
+        stmt = select(
+                TransactionsSchemaDB
+            ).where(
+                TransactionsSchemaDB.transaction_id == transaction_id
+            )
+        transaction = ses.scalar(stmt)
+
+    if transaction:
+        return transaction
+
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"there is no transaction with {transaction_id=}"
         )
     
+
+def update_user_balance(
+    user_id: int,
+    db_session: Any,
+    delta_res1: int,
+    delta_res2: int
+):
+    stmt = update(
+                UsersSchemaDB
+            ).where(
+                UsersSchemaDB.user_id == user_id
+            ).values(
+                res1 = UsersSchemaDB.res1 + delta_res1,
+                res2 = UsersSchemaDB.res2 + delta_res2,
+            )
+
+    db_session.execute(stmt)
 
 
 @router.post("/")
@@ -84,32 +108,25 @@ def create_transaction(
 
     if (not is_admin) and (requester_uid != str(transaction.user_from_id)):
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="THAT'S ILLEGAL"
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "THAT'S ILLEGAL"
         )
-    
 
     try:
         with Session(sql_engine) as ses:
-            stmt = update(
-                UsersSchemaDB
-            ).where(
-                UsersSchemaDB.user_id == transaction.user_from_id
-            ).values(
-                res1 = UsersSchemaDB.res1 - transaction.res1,
-                res2 = UsersSchemaDB.res2 - transaction.res2,
-            )        
-            ses.execute(stmt)
+            update_user_balance(
+                user_id    = transaction.user_from_id,
+                db_session = ses,
+                delta_res1 = (-1) * transaction.res1,
+                delta_res2 = (-1) * transaction.res2 
+            )
 
-            stmt = update(
-                UsersSchemaDB
-            ).where(
-                UsersSchemaDB.user_id == transaction.user_to_id
-            ).values(
-                res1 = UsersSchemaDB.res1 + transaction.res1,
-                res2 = UsersSchemaDB.res2 + transaction.res2,
-            )        
-            ses.execute(stmt)
+            update_user_balance(
+                user_id    = transaction.user_to_id,
+                db_session = ses,
+                delta_res1 = transaction.res1,
+                delta_res2 = transaction.res2 
+            )
 
             transaction_obj = TransactionsSchemaDB(**dict(transaction))
             stmt = ses.add(transaction_obj)
@@ -120,7 +137,7 @@ def create_transaction(
 
     except Exception as ex:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail = f"can't create transaction: {ex}"    
         )
 
@@ -135,36 +152,33 @@ def delete_transaction(
 ):
     if not is_admin:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="only admins can delete transactions"
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "only admins can delete transactions"
         )
 
     try:
         with Session(sql_engine) as ses:
             
-            stmt = select(TransactionsSchemaDB).where(TransactionsSchemaDB.transaction_id==transaction_id)
+            stmt = select(
+                    TransactionsSchemaDB
+                ).where(
+                    TransactionsSchemaDB.transaction_id == transaction_id
+                )
             transaction = ses.scalar(stmt)
 
-            stmt = update(
-                UsersSchemaDB
-            ).where(
-                UsersSchemaDB.user_id == transaction.user_from_id
-            ).values(
-                res1 = UsersSchemaDB.res1 + transaction.res1,
-                res2 = UsersSchemaDB.res2 + transaction.res2,
-            )        
-            ses.execute(stmt)
+            update_user_balance(
+                user_id    = user_from_id,
+                db_session = ses,
+                delta_res1 = transaction.res1,
+                delta_res2 = transaction.res2
+            )
 
-            stmt = update(
-                UsersSchemaDB
-            ).where(
-                UsersSchemaDB.user_id == transaction.user_to_id
-            ).values(
-                res1 = UsersSchemaDB.res1 - transaction.res1,
-                res2 = UsersSchemaDB.res2 - transaction.res2,
-            )        
-            ses.execute(stmt)
-
+            update_user_balance(
+                user_id    = user_to_id,
+                db_session = ses,
+                delta_res1 = (-1) * transaction.res1,
+                delta_res2 = (-1) * transaction.res2
+            )
 
             stmt = delete(
                 TransactionsSchemaDB
@@ -177,7 +191,7 @@ def delete_transaction(
 
     except Exception as ex:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail = f"can't delete transaction: {ex}"    
         )
 
